393ac242aa616701f3b825c26a8e40eed8f629e1
JBeluche <j.beluche@outlook.com>
2025-11-13 10:53:43 +0100

    SingleChildScrollView

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -14,69 +14,72 @@ class _ProfilePageState extends State<ProfilePage> {
   double sliderValue = 0.0;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: nameController,
-            decoration: InputDecoration(
-              labelText: 'Name',
-              border: OutlineInputBorder(),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                border: OutlineInputBorder(),
+              ),
+              onEditingComplete: () => setState(() {}),
             ),
-            onEditingComplete: () => setState(() {}),
-          ),
-          Text(nameController.text),
-
-          Checkbox.adaptive(
-            tristate: true,
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          CheckboxListTile.adaptive(
-            tristate: true,
-            title: const Text('Accept Terms'),
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          Switch.adaptive(
-            value: isSwitched,
-            onChanged: (bool value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-          ),
-          SwitchListTile.adaptive(
-            value: isSwitched,
-            onChanged: (bool value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-            title: const Text('Enable Notifications'),
-          ),
-          Slider.adaptive(
-            value: sliderValue,
-            divisions: 10,
-            onChanged: (double value) {
-              setState(() {
-                sliderValue = value;
-              });
-              print(value);
-            },
-            min: 0,
-            max: 100,
-          ),
-        ],
+            Text(nameController.text),
+      
+            Checkbox.adaptive(
+              tristate: true,
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile.adaptive(
+              tristate: true,
+              title: const Text('Accept Terms'),
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            Switch.adaptive(
+              value: isSwitched,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            SwitchListTile.adaptive(
+              value: isSwitched,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+              title: const Text('Enable Notifications'),
+            ),
+            Slider.adaptive(
+              value: sliderValue,
+              divisions: 10,
+              onChanged: (double value) {
+                setState(() {
+                  sliderValue = value;
+                });
+                print(value);
+              },
+              min: 0,
+              max: 100,
+            ),
+            Image.asset('assets/images/man.jpg'),
+          ],
+        ),
       ),
     );
   }
