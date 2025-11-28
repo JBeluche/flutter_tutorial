5ff5bd581146508a520ebe45ff80a694ef691caa
JBeluche <j.beluche@outlook.com>
2025-11-28 09:56:54 +0100

    Layout builder

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -5,14 +5,18 @@ import 'package:lottie/lottie.dart';
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key, required this.title});
 
-final String title;
+  final String title;
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController controllerEmail = TextEditingController(text: 'jeremie');
-  TextEditingController controllerPassword = TextEditingController(text: 'beluche');
+  TextEditingController controllerEmail = TextEditingController(
+    text: 'jeremie',
+  );
+  TextEditingController controllerPassword = TextEditingController(
+    text: 'beluche',
+  );
   String confirmedEmail = 'jeremie';
   String confirmedPassword = 'beluche';
 
@@ -37,52 +41,53 @@ class _LoginPageState extends State<LoginPage> {
       body: Center(
         child: SingleChildScrollView(
           child: Padding(
-
             padding: const EdgeInsets.all(20.0),
-            child: FractionallySizedBox(
-              widthFactor: 0.5,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Lottie.asset('assets/lotties/house.json', height: 400.0),
-              
-                  SizedBox(height: 20),
-                  TextField(
-                    controller: controllerEmail,
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+            child: LayoutBuilder(
+              builder: (context, BoxConstraints constraints) {
+                return FractionallySizedBox(
+                  widthFactor: constraints.maxWidth > 500 ? 0.5 : 1.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Lottie.asset('assets/lotties/house.json', height: 400.0),
+
+                      SizedBox(height: 20),
+                      TextField(
+                        controller: controllerEmail,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                        onEditingComplete: () => setState(() {}),
                       ),
-                    ),
-                    onEditingComplete: () => setState(() {}),
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    
-                    controller: controllerPassword,
-                    decoration: InputDecoration(
-                      
-                      hintText: 'Password',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+                      SizedBox(height: 10),
+                      TextField(
+                        controller: controllerPassword,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                        ),
+                        onEditingComplete: () => setState(() {}),
                       ),
-                    ),
-                    onEditingComplete: () => setState(() {}),
+                      SizedBox(height: 20.0),
+                      FilledButton(
+                        style: FilledButton.styleFrom(
+                          minimumSize: Size(double.infinity, 50),
+                        ),
+                        onPressed: () {
+                          onLoginPressed();
+                        },
+                        child: Text(widget.title),
+                      ),
+                      SizedBox(height: 50.0),
+                    ],
                   ),
-                  SizedBox(height: 20.0),
-                  FilledButton(
-                    style: FilledButton.styleFrom(
-                      minimumSize: Size(double.infinity, 50),
-                    ),
-                    onPressed: () {
-                      onLoginPressed();
-                    },
-                    child: Text(widget.title),
-                  ),
-                  SizedBox(height: 50.0),
-                ],
-              ),
+                );
+              },
             ),
           ),
         ),
